e4281f380f8635fc315265ed0992e86a8e746913
icaapriliani <icaaprilianikd.com>
2026-03-14 22:17:21 +0700

    membuat kerangka dasar Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,48 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Replika BRImo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00529C)),
+        useMaterial3: true,
+
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body:const SafeArea(
+        child: SingleChildScrollView(
+        child:Column(
+          children: [
+            SizedBox(height: 50),
+            Center(
+              child:Text('kanvas replika brimo'),
+            ),
+
+        ],
+        ),
+      ),
+      ),
+      );
+    
+  }
+}
