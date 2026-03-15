fd151f4c6bfcf6faa088f5223294487b42928ec1
icaapriliani <icaaprilianikd.com>
2026-03-15 11:21:06 +0700

    membuat layout Header utama warna biru

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00529C)),
         useMaterial3: true,
-
       ),
       home: const HomePage(),
     );
@@ -29,20 +28,65 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body:const SafeArea(
-        child: SingleChildScrollView(
-        child:Column(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            SizedBox(height: 50),
-            Center(
-              child:Text('kanvas replika brimo'),
-            ),
+            Container(
+              height: 220,
+              padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+              decoration: const BoxDecoration(
+                color: Color(0xFF00529C),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Selamat Datang,',
+                        style: TextStyle(color: Colors.white70, fontSize: 14),
+                      ),
+                      Text(
+                        'ica',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
 
-        ],
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.notifications_none,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.power_settings_new,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-      ),
-      );
-    
+    );
   }
 }
